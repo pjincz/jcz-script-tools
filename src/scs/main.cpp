84c212736e258b551b43a9cb5608569858984a34
jinchizhong <jinchizhong@kingsoft.com>
2012-08-04 04:00:29 +0800

    建立monitor相关类

--- src/scs/main.cpp
+++ src/scs/main.cpp
@@ -9,18 +9,74 @@ QTextStream g_sin(stdin);
 QTextStream g_sout(stdout);
 QTextStream g_serr(stderr);
 
+void report(const QString & str)
+{
+	g_sout << str << endl;
+}
+
+class SCSINotifyThread : public QThread
+{
+	Q_OBJECT
+public:
+	SCSINotifyThread()
+	{
+	}
+	void run()
+	{
+        sleep(5);
+		emit onMonitorEstablished();
+		sleep(5);
+	}
+signals:
+	void onFileAdded(QString filePath);
+	void onFileRemoved(QString filePath);
+	void onMonitorEstablished();
+};
+
+class SCSINotifyServ : public QObject
+{
+	Q_OBJECT
+public:
+	SCSINotifyServ(QObject * parent)
+		: QObject(parent)
+	{
+		QEventLoop emptyLoop;
+		connect(&m_thread, SIGNAL(onFileAdded(QString))
+				, this, SIGNAL(onFileAdded(QString)));
+		connect(&m_thread, SIGNAL(onFileRemoved(QString))
+				, this, SIGNAL(onFileRemoved(QString)));
+		connect(&m_thread, SIGNAL(onMonitorEstablished())
+				, &emptyLoop, SLOT(quit()));
+
+		m_thread.start();
+		report("Wait for establish monitor.");
+		emptyLoop.exec(QEventLoop::ExcludeUserInputEvents
+					 | QEventLoop::ExcludeSocketNotifiers
+					 | QEventLoop::WaitForMoreEvents);
+		report("Monitor established.");
+	}
+signals:
+	void onFileAdded(QString filePath);
+	void onFileRemoved(QString filePath);
+private:
+	SCSINotifyThread m_thread;
+};
+
 class SCSServer : public QObject
 {
 	Q_OBJECT
 public:
 	SCSServer()
 	{
+		m_inotifyServ = new SCSINotifyServ(this);
 	}
 public slots:
 	void quit()
 	{
 		qCoreApp->exit(0);
 	}
+private:
+	SCSINotifyServ * m_inotifyServ;
 };
 
 int server_main(QStringList args)
